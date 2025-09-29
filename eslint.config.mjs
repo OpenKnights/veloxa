110dbdcb2c3435a5cf14b8c8abfe9dffc42bb8fd
Coder-King3 <2196592083@qq.com>
2025-09-29 22:04:24 +0800

    update rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,7 @@
 import king3 from '@king-3/eslint-config'
 
-const eslintConfig = king3(
-  {
-    typescript: true
-  },
-  {
-    name: 'custom-rules',
-    rules: {
-      /* regpexp */
-      'regexp/no-unused-capturing-group': 'off'
-    }
-  }
-)
+const eslintConfig = king3({
+  typescript: true
+})
 
 export default eslintConfig
