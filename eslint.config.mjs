143f7d0c0b0e6acea4d49e7e224099859e2321ad
Coder-King3 <2196592083@qq.com>
2025-09-29 23:35:43 +0800

    change eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,14 @@
 import king3 from '@king-3/eslint-config'
 
-const eslintConfig = king3({
-  typescript: true
-})
+const eslintConfig = king3(
+  {
+    typescript: true
+  },
+  {
+    rules: {
+      'regexp/no-unused-capturing-group': 'off'
+    }
+  }
+)
 
 export default eslintConfig
