6c68dc195047d39ac9af384a5fb7a0059bfeaddf
King-3 <75344974+coderking3@users.noreply.github.com>
2025-11-26 08:28:32 +0000

    Remove comments

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,3 @@ export type * from './types'
 export * from './veloxa'
 
 export const veloxa = createVeloxa()
-
-// import { createFetch } from "./base.ts";
-// import type { $Fetch } from "./types.ts";
-
-// export * from "./base.ts";
-
-// export type * from "./types.ts";
-
-// // Allow patching globalThis.fetch
-// export const fetch: typeof globalThis.fetch = globalThis.fetch
-//   ? (...args: Parameters<typeof globalThis.fetch>) => globalThis.fetch(...args)
-//   : () => Promise.reject(new Error("[ofetch] global.fetch is not supported!"));
-
-// export const ofetch: $Fetch = createFetch({ fetch });
-
-// export const $fetch: $Fetch = ofetch;
